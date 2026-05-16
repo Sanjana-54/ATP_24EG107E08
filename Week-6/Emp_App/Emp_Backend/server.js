7adf349544d61f8a3ce2d78cd94cbc7f68fbbe14
Sanjana <sanjanagoud054@gmail.com>
2026-05-16 17:25:56 +0530

    updated files

--- Week-6/Emp_App/Emp_Backend/server.js
+++ Week-6/Emp_App/Emp_Backend/server.js
@@ -13,7 +13,7 @@ const app=exp();
 app.use(
   cors({
     origin:["http://localhost:5173",
-      "https://employeemanagement-coral.vercel.app/"
+      "https://employeemanagement-coral.vercel.app"
     ],
     credentials:true
   }),
